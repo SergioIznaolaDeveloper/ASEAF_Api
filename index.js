bc1b42e6b3eb912f4947de38d09ddd4cf3c0c685
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-19 13:08:58 +0200

    First deployment setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv');
 const cors = require('cors')
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 // Routes:
 const routes = require('./routes/api_routes');
