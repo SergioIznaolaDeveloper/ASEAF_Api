a1a2789612ea983c3168fa138565d3741dd06142
SergioIznaolaDeveloper <86465984+SergioIznaolaDeveloper@users.noreply.github.com>
2022-05-19 19:07:21 +0200

    [ADD]login_form_created

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv');
-
 const cors = require('cors')
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware:
 app.use(cors());
