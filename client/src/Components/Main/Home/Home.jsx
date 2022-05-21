cba3a5bec7cabcb44b44aff21dd1fc8ef0ba6b2b
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-21 12:54:48 +0200

    [FIX] updated formularies routes to match heroku deployment url

--- client/src/Components/Main/Home/Home.jsx
+++ client/src/Components/Main/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
     return (
       <div className="loginPadre">
         <p>Signup</p>
-        <form method="POST" action="http://localhost:5000/api/signup" >
+        <form method="POST" action="/api/signup" >
           <label>Email</label>
           <input type="text" name="email" placeholder="Name" />
           <label>Pass</label>
@@ -14,7 +14,7 @@ function Home () {
           <button type="submit">Signup</button>
         </form>
         <p>Login</p>
-        <form form method="POST" action="http://localhost:5000/api/login">
+        <form form method="POST" action="/api/login">
           <label>Email</label>
           <input type="text" name="email" placeholder="Name"/>
           <label>Pass</label>
