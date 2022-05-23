2a63b493e857d868c2d8263d15fc4b81b5e71b73
Gaizka79 <agaizka79@gmail.com>
2022-05-23 17:48:01 +0200

    [ADD] bt navegacion

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import btHeader from '../../Assets/Icons/btHeader.png';
 
 function Header() {
   
+
     return (
       <div className="header"> 
-      <nav className="header__nav">
-        <Link className="header__link" to='/tickets'>TICKETS</Link>
-        <Link className="header__link" to='/filtros'>FILTROS</Link>
-      </nav>
+        <nav className="header__nav">
+          <div className="dropdown">
+            <img src={btHeader} alt="imagen de boton" />
+            <div className="dropdown-content">
+              <a href="#">Sobre Nosotros</a>
+              <a href="#">Acogimiento familiar</a>
+              <a href="#">Qué puedes hacer tú</a>
+              <a href="#">Actividad</a>
+              <a href="#">Contacto</a>
+            </div>
+          </div>
+            <Link className="header__link" to='/tickets'>TICKETS</Link>
+            <Link className="header__link" to='/filtros'>FILTROS</Link>
+        </nav>
       </div>
     )
 }
