1dcdbfa5175a31c6b4a2d85eee095d7dd3f109d4
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-25 12:25:55 +0200

    [FIX] updated filter controller: it was not returning appropiate response

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -17,13 +17,11 @@ const createNewTicket = async (req, res, next) => {
 
 const getFilteredTickets = async (req, res, next) => {
     try {
-        // const tickets = await getTickets(req.body);
-        // if (!tickets || tickets.length < 1) {
-        //     return res.status(400).json({ response: false, message: 'No tickets found' });
-        // }
-        // res.status(200).json({ response: true, authenticated: true, data: tickets })
-        console.log(req.body)
-        res.status(200).json({ response: req.body })
+        const tickets = await getTickets(req.body);
+        if (!tickets || tickets.length < 1) {
+            return res.status(400).json({ response: false, message: 'No tickets found' });
+        }
+        res.status(200).json({ response: true, authenticated: true, data: tickets })
     }
     catch (error) {
         return next(error)
