c1a4e4838f2147260281ebf2889e503cfaa513aa
Gaizka79 <agaizka79@gmail.com>
2022-05-25 16:59:52 +0200

    [ADD] bt Navegacion error-fixed

--- client/src/Components/Header/Nav/Nav.jsx
+++ client/src/Components/Header/Nav/Nav.jsx
@@ -1,46 +1,25 @@
-import React, {useState} from "react";
-import btHeader from '../../../Assets/Icons/btHeader.png';
-import btX from '../../../Assets/Icons/btX2.png';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  const [ toggleBt, setToggleBt ] = useState(false);
-
-    return (
-      <>
-        <img class="menu__btn" className="btNav" onClick={()=>setToggleBt(!toggleBt)} src={btHeader} alt="imagen de boton" />
-        <nav className="header__nav">
-     
-              
-              <ul className={toggleBt? "dropdown-content-show" : "dropdown-content"}>
-                <img className="btX" onClick={()=>setToggleBt(false) } src={btX} alt="boton cerrar" />
-                <li><a className="header_item" href="#">Sobre Nosotros</a></li>
-                <li><a className="header_item" href="#">Acogimiento familiar</a></li>
-                <li><a className="header_item" href="#">Forfamily</a></li>
-                <li><a className="header_item" href="#">Qué puedes hacer tú</a></li>
-                <li><a className="header_item" href="#">Actividad</a></li>
-                <li><a className="header_item" href="#">Contacto</a></li>
-              </ul>
-            
-          </nav>
-      </>
-    )
-  
+  return (
+    <>
+      <div className="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label className="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
+        <ul className="menu__box">            
+          <li><Link className="menu__item" to="#">Sobre nosotros</Link></li>
+          <li><Link className="menu__item" to="#">Acogimiento</Link></li>
+          <li><Link className="menu__item" to="#">Forfamily</Link></li>
+          <li><Link className="menu__item" to="#">Qué puedes hacer tú</Link></li>
+          <li><Link className="menu__item" to="#">Actividad</Link></li>
+          <li><Link className="menu__item" to="#">Contacto</Link></li>
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default Nav;
-/*
-<nav className="header__nav">
-          <div className="dropdown">
-            <img src={btHeader} alt="imagen de boton" />
-            <ul className="dropdown-content">
-              <li><a href="#">Sobre Nosotros</a></li>
-              <li><a href="#">Acogimiento familiar</a></li>
-              <li><a href="#">Forfamily</a></li>
-              <li><a href="#">Qué puedes hacer tú</a></li>
-              <li><a href="#">Actividad</a></li>
-              <li><a href="#">Contacto</a></li>
-            </ul>
-          </div>
-        </nav>
-        */
