53d13bd4cb47b5bccf9431bed5625b6c84707f75
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-05-26 23:22:53 +0200

    [FIX] added log out functionality (front only)

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -5,34 +5,22 @@ import Nav from "./Nav/Nav";
 import { LoginContext } from '../../Context/Login';
 
 function Header() {
-  
-  const { isAuthenticated } = useContext(LoginContext);
 
-    return (
-      <div className="header"> 
-        <Nav/>
-        <Link className="header__logo" to='/'></Link>
-        <Link className="header__link" to='/login'><button className="header__button">{isAuthenticated ? 'Log Out' : 'Acceder'}</button></Link>
-      </div>
-    )
+  const { isAuthenticated, setIsAuthenticated } = useContext(LoginContext);
+
+  const handleLogOut = () => {
+    if (isAuthenticated) {
+      setIsAuthenticated(false);
+      localStorage.setItem('auth', false);
+    }
+  }
+
+  return (
+    <div className="header">
+      <Nav />
+      <Link className="header__logo" to='/'></Link>
+      <Link className="header__link" to='/login'><button className="header__button" onClick={handleLogOut}>{isAuthenticated ? 'Log Out' : 'Acceder'}</button></Link>
+    </div>
+  )
 }
 export default Header;
-/*
-<div className="header"> 
-  <nav className="header__nav">
-    <div className="dropdown">
-      <img src={btHeader} alt="imagen de boton" />
-      <div className="dropdown-content">
-        <a href="#">Sobre Nosotros</a>
-        <a href="#">Acogimiento familiar</a>
-        <a href="#">Forfamily</a>
-        <a href="#">Qué puedes hacer tú</a>
-        <a href="#">Actividad</a>
-        <a href="#">Contacto</a>
-      </div>
-    </div>
-      <Link className="header__link" to='/tickets'>TICKETS</Link>
-      <Link className="header__link" to='/filtros'>FILTROS</Link>
-  </nav>
-</div>
-      */
